392f12626bcca0470cb8a3e25b0697bf735b38f4
marcoaam <marco.army@gmail.com>
2014-06-26 10:34:44 +0100

    Working version #10

--- directory.rb
+++ directory.rb
@@ -39,7 +39,7 @@ def print_header
 end
 
 #Hash Input method
-def input_students(students)
+def input_students
   puts "Please enter the name of the first student"
   puts "To go back to the Main menu, just hit return without entering any name"
   #Get the 1st name
@@ -56,32 +56,30 @@ def input_students(students)
     puts "Enter their hobbies"
     hobbies = gets.chomp
     #add the student hash to the array
-    students << {:name => name, :cohort => month, :hobbies => hobbies}
+    @students << {:name => name, :cohort => month, :hobbies => hobbies}
     puts number_of_students(students)
     #get another name from the user
     puts "\nEnter the name of the next student or press return to go back to the Main menu"
     name = gets.chomp
   end
-  #returns the array of students
-  students
 end
 
-def pluralize?(students)
-  students.length > 1
+def pluralize?
+  @students.length > 1
 end
 
-def number_of_students(students)
-  if pluralize?(students)
-    "Now we have #{students.length} students"
+def number_of_students
+  if pluralize?
+    "Now we have #{@students.length} students"
   else 
-    "Now we have #{students.length} student"
+    "Now we have #{@students.length} student"
   end
 end
 
 #names method takes an array (which is an array multiple 2 element hashes) and prints each element of the array along with its index number
-def print_names(students)
-  if !students.empty? 
-    students.each.with_index(1) do |student, index|
+def print_names(list_of_students)
+  if !list_of_students.empty? 
+    list_of_students.each.with_index(1) do |student, index|
       puts "#{index}. #{student[:name]} - #{student[:cohort]} cohort, hobbies: #{student[:hobbies]}"
     end
   else
@@ -90,14 +88,14 @@ def print_names(students)
 end
 
 #This is the method that sorts the students by cohort
-def names_by_cohort(students)
-  students.sort_by { |student| student[:cohort] }
+def names_by_cohort
+  @students.sort_by { |student| student[:cohort] }
 end
 
 #only prints those elements of the students array that begin with an A
-def print_names_begins_with(students, letter)
+def print_names_begins_with(letter)
   puts "\nThis is a list of students whose names begin with the letter #{letter}"
-  name_begin_a = students.select {|student| student[:name].start_with?(letter, letter.upcase)}
+  name_begin_a = @students.select {|student| student[:name].start_with?(letter, letter.upcase)}
 #This code tests if is empty
   if name_begin_a.empty?
     puts "-There are no students whose names begin with an 'A'"
@@ -107,9 +105,9 @@ def print_names_begins_with(students, letter)
 end
 
 #only prints those elements of the students array that have less than 12 characters
-def print_length_less_12(students)
+def print_length_less_12
   puts "\nThis is a list of students with less than 12 letters in their names"
-  name_less_than_12_letters = students.select { |student| student[:name].length < 12 }
+  name_less_than_12_letters = @students.select { |student| student[:name].length < 12 }
   
   if name_less_than_12_letters.empty?
     puts "-There are no students who have less than 12 letters in their name"
@@ -119,21 +117,15 @@ def print_length_less_12(students)
 end
 
 #prints the total number of elements in the students array i.e. total number of students
-def print_footer(students)
-  if pluralize?(students)
-    puts "\nOverall, we have #{students.length} students in all cohorts"
+def print_footer
+  if pluralize?
+    puts "\nOverall, we have #{@students.length} students in all cohorts"
   else
-    puts "\nOverall, we don't have #{students.length} any student in the directory"
+    puts "\nOverall, we don't have #{@students.length} any student in the directory"
   end
 end
 
-
-
-#This method holds the main menu that calls all the methods selected by the user
-def interactive_menu(students)
-  puts ""
-  puts "-----Student Directory Program-----".center(50)
-  loop do
+def print_menu_options
     puts ""
     puts "-------Main menu-------".center(50)
     puts "1. Add Students"
@@ -142,38 +134,52 @@ def interactive_menu(students)
     puts "4. Show all the students that their names have less than 12 characters"
     puts "5. Show the students grouped by cohort"
     puts "6. Save the students in a file"
+    puts "7. Load students from the file"
     puts "9. Exit"
+end
+
+
+#This method holds the main menu that calls all the methods selected by the user
+def interactive_menu
+  puts ""
+  puts "-----Student Directory Program-----".center(50)
+  loop do
+    print_menu_options
     selection = gets.chomp
-    case selection
+    menu_options(selection)
+  end
+end
+
+def menu_options(selection)
+     case selection
     when "1"
-      students = input_students(students)
+      @students = input_students
     when "2"
       puts "\nThis is a list of all students in the Student Directory"
-      print_names(students)
-      print_footer(students)
+      print_names(@students)
+      print_footer
     when "3"
-      print_names_begins_with(students, 'a')
+      print_names_begins_with('a')
     when "4"
-      print_length_less_12(students)
+      print_length_less_12
     when "5"
       puts "\nThis is the Student Directory grouped by cohort month"
-      sorted_students = names_by_cohort(students)
+      sorted_students = names_by_cohort
       print_names(sorted_students)
     when "6"
-      save_students(students)
+      save_students
+    when "7"
+      load_students
     when "9"
       exit
     else
       puts "Invalid selection"
     end
-  end
 end
 
-def save_students(students)
-  # open the file to write the info
+def save_students
   file = File.open("students.csv", "w")
-  # iterate over the array
-  students.each do |student|
+  @students.each do |student|
     student_data = [student[:name], student[:cohort], student[:hobbies]]
     csv_line = student_data.join(",")
     file.puts csv_line
@@ -181,11 +187,36 @@ def save_students(students)
   file.close
 end
 
+def load_students(filename = "students.csv")
+  file = File.open("students.csv", "r")
+  file.readlines.each do |line|
+    name, cohort, hobbies = line.strip.split(',')
+      add_student(name, cohort, hobbies)
+  end
+  file.close
+end
+
+def add_student(name, cohort, hobbies)
+  @students << {name: name, cohort: cohort.to_sym, hobbies: hobbies}
+end
+def try_load_students
+  filename = ARGV.first
+  return if filename.nil?
+  if File.exists?(filename)
+    load_students(filename)
+    puts "Loaded #{students.length} students from #{filename}"
+  else
+    puts "Sorry #{filename} does not exist"
+    exit
+  end
+end
+
 #Declaration
-students = []
+@students = []
 
 #Calling the Menu method
-interactive_menu(students)
+try_load_students
+interactive_menu
 
 
 #This code calls the methods above
